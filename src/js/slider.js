b378225e045f4678a4e669dd7a6d17065f596585
Jordi <67053157+JordiMesas@users.noreply.github.com>
2020-12-01 14:05:22 +0100

    update (01/12/2020)

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,23 @@
-document.getElementById('buttons').addEventListener('click', (e) => {
-	let buttons = document.getElementsByClassName('main__button');
-	for (let i = 0; i < buttons.length; i++) {
-        buttons[i].style.backgroundColor = '#bbb';                    
-    }    
-    e.target.style.backgroundColor = '#717171';
-    let number = e.target.getAttribute('name');
-    console.log(document.getElementsByClassName('main__img')[0].src);
-    document.getElementsByClassName('main__img')[0].src = `./src/img/galery_${number}.png`
-});
+
+let buttons = document.getElementsByClassName('main__button');
+
+
+for (let i = 0; i < buttons.length; i++) {    
+    
+    buttons[i].addEventListener('click', (e) => {
+        
+        let buttons = document.getElementsByClassName('main__button');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].style.backgroundColor = '#bbb';            
+        }       
+		e.target.style.backgroundColor = '#717171';
+		let number = e.target.getAttribute('name');
+		console.log(document.getElementsByClassName('main__img')[0].src);
+		document.getElementsByClassName(
+			'main__img'
+		)[0].src = `./src/img/galery_${number}.png`;
+    });    
+
+}
 
 
