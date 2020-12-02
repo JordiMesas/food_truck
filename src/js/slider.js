5c68dbb2955d705774d25de497fd51386d35cf6c
Jordi <67053157+JordiMesas@users.noreply.github.com>
2020-12-02 14:48:58 +0100

    update slider (02/12/2020)

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,23 +1,38 @@
+buttons = document.getElementsByClassName('main__button');
 
-let buttons = document.getElementsByClassName('main__button');
+// for (let i = 0; i < buttons.length; i++) {
+// 	buttons[i].addEventListener('click', (e) => {
+// 		number = e.target.getAttribute('name');
+// 		changeImg(number);
+// 	});
+// }
 
+i=2;
 
-for (let i = 0; i < buttons.length; i++) {    
-    
-    buttons[i].addEventListener('click', (e) => {
-        
-        let buttonsInitial = document.getElementsByClassName('main__button');
-        for (let i = 0; i < buttonsInitial.length; i++) {
-            buttonsInitial[i].style.backgroundColor = '#bbb';            
-        }       
-		e.target.style.backgroundColor = '#717171';
-		let number = e.target.getAttribute('name');
-		console.log(document.getElementsByClassName('main__img')[0].src);
-		document.getElementsByClassName(
-			'main__img'
-		)[0].src = `./src/img/galery_${number}.png`;
-    });    
+document.getElementById('left').addEventListener('click', () => {
+	i--;
+	if (i < 0) {
+		i = 0;
+	}
+	changeImg(i);
+});
 
-}
+document.getElementById('right').addEventListener('click', () => {
+	i++;
+	if (i > 4) {
+		i = 4;
+	}
+	changeImg(i);
+});
 
+const changeImg = (i) => {
+	console.log(i);
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].style.backgroundColor = '#bbb';
+	}
+	buttons[i].style.backgroundColor = '#717171';
 
+	document.getElementsByClassName(
+		'main__img'
+	)[0].src = `./src/img/galery_${i}.png`;
+};
